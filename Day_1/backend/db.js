3a91f6a0bb72ab2264e391f80868261e94299f5a
sivzz-22 <sivasaravanakumarm@gmail.com>
2025-12-09 11:20:32 +0530

    Food_App Completed

--- Day_1/backend/db.js
+++ Day_1/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri = 'mongodb+srv://sujith:9345793342S@cluster0.dquerwy.mongodb.net/'
+const uri = 'mongodb+srv://sivafullstack:siva2239@cluster0.j4mtaw4.mongodb.net/'
 mongoose.connect(uri)
 const connection = mongoose.connection
 
